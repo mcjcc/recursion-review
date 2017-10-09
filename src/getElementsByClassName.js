0f1f7ebcb006307a1204b0b202105e20bfc8c91b
mcjcc <mcjohnnychen@gmail.com>
2017-10-09 15:32:16 -0700

    Finish getElementsByClassName with inner recursive function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,42 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+
+var getElementsByClassName = function(className) {
   // your code here
+
+  // base case
+  var body = document.body;
+
+  var classArray = [];
+
+  var getElements = function(element) {
+    
+    // var list = Array.prototype.slice.call(element.classList, 0);
+    var foundName = false;
+    
+    if (element.classList !== undefined) {
+      for (var j = 0; j < element.classList.length; j++) {
+        if (className === element.classList[j]) {
+          foundName = true;
+        }      
+      }
+    }
+
+    if (foundName === true) {
+      classArray.push(element);
+    }
+    
+    for (var i = 0; i < element.childNodes.length; i++) {
+      getElements(element.childNodes[i]);
+    }
+  };
+
+  getElements(body);
+  
+  // returns an array with elements with class name
+
+  return classArray;
+
 };
